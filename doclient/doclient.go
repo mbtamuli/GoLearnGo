b3e7796e609552db844cda51644ad34580f58111
Mriyam Tamuli <mbtamuli@gmail.com>
2019-01-03 09:29:56 +0530

    Extend doclient
    1. Test multi droplet creation
    2. Test deletion with tags
    3. Flesh out list droplets
    
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- doclient/doclient.go
+++ doclient/doclient.go
@@ -17,8 +17,13 @@ func main() {
 	}
 	bearer := "Bearer " + doToken
 
+	/*
 	var jsonStr = []byte(`{
-		"name": "example",
+		// "name": "one"
+		"names": [
+			"two",
+			"three"
+		],
 		"region": "blr1",
 		"size": "s-1vcpu-1gb",
 		"image": "ubuntu-18-04-x64",
@@ -41,10 +46,53 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+	fmt.Println(resp.StatusCode)
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic(err.Error())
 	}
 	fmt.Println(string([]byte(body)))
+	*/
 
+
+	type Droplet struct {
+		id int
+		name string
+	}
+
+	req, err := http.NewRequest(http.MethodGet, baseurl+"v2/droplets", bytes.NewBuffer(jsonStr))
+	req.Header.Add("Authorization", bearer)
+	req.Header.Add("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(resp.StatusCode)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	var d Droplet
+	err := json.Unmarshal([]byte(body), &d)
+	fmt.Println(d.name)
+
+
+	// Delete droplet /v2/droplets/$DROPLET_ID
+	// Delete droplet with tag /v2/droplets?tag_name=$TAG_NAME
+	/*
+	req, err := http.NewRequest(http.MethodDelete, baseurl+"v2/droplets?tag_name=mbtamuli", nil)
+	req.Header.Add("Authorization", bearer)
+	req.Header.Add("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err.Error())
+	}
+	if resp.StatusCode == 204 {
+		fmt.Printf("Successfully deleted droplets with tag mbtamuli")
+	} else {
+		panic("Failure")
+	}
+	*/
 }
